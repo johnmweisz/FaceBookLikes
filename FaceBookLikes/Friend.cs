d72122961f89c6d7905c94bff66ccac6218f053a
John Weisz <johnmweisz@gmail.com>
2019-12-11 11:18:33 -0800

    Add project files.

---
+++ FaceBookLikes/Friend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FaceBookLikes
+{
+    public class Friend
+    {
+        private string Name;
+        private Byte Age;
+        public Friend(string name, byte age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Info()
+        {
+            return $"Name {Name}, Age {Age}";
+        }
+    }
+}
---
+++ FaceBookLikes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceBookLikes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var likes = new List<Friend>
+            {
+                new Friend("John", 22),
+                new Friend("Mai", 22),
+                new Friend("Julie", 22)
+            };
+            string result = "";
+            for (int i = 0; i < Math.Min(likes.Count(), 3); i++)
+            {
+                if (i < 2) result += likes[i].Info() + " ";
+                else result += $"and {likes.Count() - 2} others";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
